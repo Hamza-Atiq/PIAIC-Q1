c4210a85ae2ee57ec1f8f98a5f8c2a194da1eba6
Hamza Atiq <hamzaatiq34@gmail.com>
2024-09-05 15:52:44 +0500

    Excersise_2_task

---
+++ Python_crash_couse_exercise_solved/chapter_2/exercise_2.py
@@ -0,0 +1,14 @@
+# 2.1 Simple Messaage
+
+simple_message_1 = "Imran Khan is a Famous Leader in the World"
+
+print(simple_message_1)
+
+simple_message_2 = 'Imran Khan changed the Pakistan Insha Allah'
+
+print(simple_message_2)
+
+simple_message_2 = 'Imran Khan is our hero'
+
+print(simple_message_2)
+
