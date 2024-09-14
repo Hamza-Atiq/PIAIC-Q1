18bde08426e7910163e2562f22e75af86e1d0517
Hamza Atiq <hamzaatiq34@gmail.com>
2024-09-14 16:41:15 +0500

    Add excercise

---
+++ Python_crash_couse_exercise_solved/chapter_2/exercise_2.py
@@ -0,0 +1,30 @@
+# Task 2.1 
+
+simple_message_1 : str = "Imran Khan is a Famous Leader in the World"
+
+print(simple_message_1 )
+
+print('*'*50)
+
+# Task 2.2 
+
+simple_message_2 : str = 'Imran Khan changed the Pakistan Insha Allah'
+
+print(simple_message_2)
+
+simple_message_2 : str  = 'Imran Khan is our hero'
+
+print(simple_message_2)
+
+print("*"*100)
+
+# Task 2.3
+
+name : str = 'Hamza Atiq'
+
+print(f'Hello {name}, would you like to learn some Python today ? ')
+
+# Task 2.4
+
+
+
